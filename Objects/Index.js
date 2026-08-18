d6b2feaf1e54d92338594018331210973a553625
juthugasaitanvi-max <juthuga.saitanvi@gmail.com>
2026-08-18 18:04:53 +0530

    Objects

---
+++ Objects/Index.js
@@ -0,0 +1,39 @@
+// Object: Its a way to group related information together all in a single place, under the same name. 
+// Steps to remember to create an object:
+// 1. use of let or const.
+// 2. open a curly bracket.
+// 3. Add key: value pairs, seperate them with commas.
+// 4. Close the curly bracket.
+// Example:
+// let student={
+//     name:"Saitanvi", 
+//     age:10,
+//     favoriteSubject:"Science"
+// }
+
+// console.log(student.name)
+// console.log(student["name"])
+
+// console.log(student.age)
+// console.log(student["age"])
+
+
+// Properties of Objects:
+// key- any thing that is on the left side of the colon(:).
+// Value- any thing that is on the right side of the colon(:).
+
+// To access a property we can  use either DOT or bracket annotation.
+let phone_contact={
+    name:"Saitanvi",
+    phone_number :"1234509876",
+    school_name:"Triumphant Institute of Management Education",
+    city:"Hyderabad",
+}
+  console.log(phone_contact.school_name)
+ 
+
+
+
+
+
+
