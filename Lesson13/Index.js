05932f37bcd940124129792616ac6b7256dcf3b3
juthugasaitanvi-max <juthuga.saitanvi@gmail.com>
2026-09-03 20:24:17 +0530

    acp.html

---
+++ Lesson13/Index.js
@@ -0,0 +1,95 @@
+// Topics to be covered:
+// Null and undefined.
+// Comparison operators.
+// If/else conditional statements.
+// Date object.
+// While loop.
+// String Concatenation.
+
+// 1. Null and Undefined:
+// Null is set by us on purpose to be empty.
+// Undefined: Its declared by the system when the variable was never provided with a value.
+
+// Example for Null & Undefined:
+
+// let name=null // I on purpose made it empty.
+// console.log(name)
+// let age; // Variable is declared but no value is provided to it.
+// console.log(age)
+
+
+// 2. Comparison Operators: These are like asking True/False about two things.They always gives us with True or False.
+// Eg for conditional & If/else : 
+// let marks1=24
+// let marks2=26
+// let total=50
+// let sum=0
+
+// sum=marks1+marks2
+
+// if(sum==total){
+//     console.log("Marks is equal to total, i.e.", sum+"/"+total)
+// } else
+// {
+//     console.log("Marks less than",total)
+// }
+
+
+// 4. Date Object: Its JavaScript's built-in clock and calendar. It lets our code know what date and time is.
+
+// let today= new Date().getHours(); // We are capturing current date and time.
+
+// let greeting;
+
+// if (today<12){
+// greeting="Good Morning"
+// }else if (today<18){
+//     greeting = "Good Afternoon"
+// }else{
+//     greeting = "Good Evening"
+// }
+
+// console.log(greeting)
+
+// 5. While loop: A while loop keeps on repeating an action as long as a condition is true- like saying: "Keep on bouncing the ball while you have energy."
+
+// Example:
+
+// let lives=3;
+
+// while(lives>0){
+//     console.log("Lives left: "+ lives)
+//     lives=lives-1
+// }
+
+// 6. String Concatenation: We join two or more strings together using the '+' sign.
+// Example:
+
+let firstName="Juthuga"
+let lastName="Saitanvi"
+
+let fullName=firstName+" "+lastName
+console.log(fullName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
