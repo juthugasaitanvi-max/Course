fc334837ee6315a76a4663cb69e8dbdddba69139
juthugasaitanvi-max <juthuga.saitanvi@gmail.com>
2026-09-25 19:23:06 +0530

    acp.py

---
+++ Python/Index.py
@@ -0,0 +1,12 @@
+print("Hello, Everyone!")
+print("My name is Saitanvi.")
+print("The result of 3+5 is ", 3+5)
+print("The result of 1029+269 is ", 1029+269)
+print("The result of 10/2000 is ", 10/2000)
+print("I very very very very good at python programming.")
+print("I love science")
+print("I love to play football")
+print("I love HTML, CSS, and JavaScript")
+#Python does not have a command for printing a new line. But we can use the end parameter of the print() function to add a new line. By default, the end parameter is set to '\n', which means that a new line will be printed after each print statement. However, we can change the value of the end parameter to any string we want.
+print("This is the first line.", end="\n")
+
---
+++ Python/acp.py
@@ -0,0 +1,49 @@
+# My Daily Routine Printer
+# Outline:
+# In this assignment, students will create a simple Python program that prints a daily routine step by step. They will learn how the print() function displays text on the screen and how arithmetic can also be printed as output.The program will print a title, show routine steps like waking up, going to school, playing, and studying, and finally calculate the total study time in a week.
+
+print("My Daily Routine")
+
+print(" Wake up at 6:00 AM")
+
+print("Brush teeth and take a shower")
+
+print(" Switch on the TV")
+ 
+print(" Have breakfast while watching the TV")
+
+print(" Go to school at 8:00 AM")
+
+print(" Listen to 1st period :Science class")
+
+print(" Roam around the class :Break time")
+
+print(" Listen to 2nd period :Maths class")
+
+print(" Listen to 3rd period :English class")
+
+print("  Listen to 4th period :Social Science class")
+
+print("  Listen to 5th period :Life Skills class")
+
+print("  Eat lunch at 12:30 PM and I get up from my bench and go to other people and talk to them")
+
+print("  Listen to 6th period :Hindi class")
+
+print("  Listen to 7th period :Games")
+
+print("  Listen to 8th period :Telugu class and I get so bored and I hide behind the front bench person and I talk")
+
+print("  Go back home at 3:30 PM")
+
+print("  Play with my friends and I play football with them")
+
+print("  Study for 3 hours from 5:00 PM to 8:00 PM")
+
+print("  Have dinner at 8:30 PM")
+
+print("  Watch TV for 30 minutes from 9:00 PM to 10:00 PM")
+
+print("  Go to bed at 10:00 PM")
+
+print("Total study time in a week is ", 3*7, "hours")
